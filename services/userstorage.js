669ae0be45c140ac3f24a990fbef2d7cd4221c82
Sundar G <sundar.gopal@yara.com>
2021-09-03 14:34:30 +0530

    started users

---
+++ services/userstorage.js
@@ -0,0 +1,74 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+let users =[
+    {id:1,name:"admin",password:"admin"},
+  ]; //default values
+
+export const getUsers = async() =>{
+    let userList = await AsyncStorage.getItem('users');
+    if (userList == null || userList =="null"){
+        users =[];
+        return [];
+    }else{
+        users =JSON.parse(userList);
+        return users;
+    }
+}
+
+const updateUsers = async() =>{
+    await AsyncStorage.setItem('users', JSON.stringify(users));
+}
+export const addUsers = async(user) =>{
+    user.id = users.length+1;
+    users.push(user);
+    await updateUsers();
+}
+
+
+
+
+
+/* export const getCustomers = async ()=>{
+    let customerList = await AsyncStorage.getItem('customers');
+    if(customerList == null || customerList == "null" ){
+        customers = [];
+        return [];
+    }else{
+        customers = JSON.parse(customerList);
+        return customers;
+    }
+}
+const updateAsyncData = async ()=>{
+    await AsyncStorage.setItem('customers',JSON.stringify(customers));
+}
+
+export const addCustomer = async(customer)=>{
+    customer.id = Date.now()+'r';
+    customers.push(customer);
+    await updateAsyncData();
+}
+
+export var deleteCustomer = async (id) =>{
+    customers = customers.filter((item)=>(item.id!=id));
+    await updateAsyncData();
+  }
+
+export const getCustomerById = (id) =>{
+    var list = customers.filter((item)=>(item.id==id));
+    if(list.length > 0){
+        return list[0];
+    }else{
+        return {}
+   }
+}
+
+export const updateCustomer = async (customer) =>{
+    var list = customers.filter((item)=>(item.id==customer.id));
+    if(list.length > 0){
+         list[0].name = customer.name;
+         list[0].email = customer.email;
+         list[0].phone = customer.phone;
+         list[0].address = customer.address;
+    }
+    await updateAsyncData();
+} */
