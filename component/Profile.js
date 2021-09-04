1d236e92503b43b92357c10d05c9d437d2c23518
Sr3yas-SK <sreyas.s@yara.com>
2021-09-04 23:18:28 +0530

    Adding Completed Profile

--- component/Profile.js
+++ component/Profile.js
@@ -1,23 +1,38 @@
 import React, { useState,useEffect } from "react";
-import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View,Button} from "react-native";
+import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View,Button ,ScrollView} from "react-native";
 
 import IconAntDesign from 'react-native-vector-icons/AntDesign'
+import IconIonicons from 'react-native-vector-icons/Ionicons' //ADD
+import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons' //ADD
+import IconFontisto from 'react-native-vector-icons/Fontisto' //ADD
+import IconSimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons' //ADD
+import IconOcticons from 'react-native-vector-icons/Octicons' //ADD
+import IconFeather from 'react-native-vector-icons/Feather' //ADD
+import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons' //ADD
 
 IconAntDesign.loadFont()
+IconIonicons.loadFont()  //ADD
+IconFontisto.loadFont()  //ADD
+IconMaterialIcons.loadFont()  //ADD
+IconSimpleLineIcons.loadFont()  //ADD
+IconOcticons.loadFont()  //ADD
+IconFeather.loadFont()  //ADD
+IconMaterialCommunityIcons.loadFont()  //ADD
 
 function profile() {
     return (
+       <ScrollView>
        <View style={styles.container}>
            
         <View> 
             
        
-        <View style={{paddingLeft:10,paddingRight:50,paddingTop:20,borderWidth:1,flexDirection: 'row'}}> 
+        <View style={{paddingLeft:10,paddingRight:50,paddingTop:20,borderWidth:0,flexDirection: 'row'}}> 
 
-        <Image style={styles.profilePic} source={require('/Users/a845596yara.com/workspace/workspace-fashion/FashionApp/src/tshirt/0001_fashion_image.jpg')}/>
+        <Image style={styles.profilePic} source={require('../src/ABC_Faishon/0001_fashion_image.jpg')}/>
        
-       <View style={{padding:8,flex:8,borderWidth:1,width:80}}>
-       <Text>fashionparade@gmail.com</Text>
+       <View style={{padding:25,flex:9,borderWidth:0}}>
+       <Text>EMAIL</Text>
        <Text></Text>
        <Text></Text>
        
@@ -33,11 +48,13 @@ function profile() {
         <View style={styles.container2} >
 
         <TouchableOpacity style={styles.Clicktabs}>
-         <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+         <View style={styles.leftIcon}>   
+        <IconAntDesign name='bars' size ={25} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:145}}>
         <Text style={styles.text} >All My Orders</Text>
-        <View style={{paddingLeft:200}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -45,11 +62,13 @@ function profile() {
         <Text></Text>
 
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconMaterialIcons name='pending-actions' size ={25} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:110}}>
         <Text style={styles.text} >Pending Shipments</Text>
-        <View style={{paddingLeft:160,borderWidth:0}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -57,11 +76,13 @@ function profile() {
         <Text></Text>
 
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconMaterialCommunityIcons name='credit-card-clock-outline' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:115}}>
         <Text style={styles.text} >Pending Payments</Text>
-        <View style={{paddingLeft:165,borderWidth:0}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -69,11 +90,13 @@ function profile() {
         <Text></Text>
 
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconSimpleLineIcons name='handbag' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:135}}>
         <Text style={styles.text} >Finished Orders</Text>
-        <View style={{paddingLeft:185,borderWidth:0}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -85,11 +108,13 @@ function profile() {
         <View style={styles.container2}>
 
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconOcticons name='mail-read' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:153}}>
         <Text style={styles.text} >Invite Friends</Text>
-        <View style={{paddingLeft:200}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -97,11 +122,13 @@ function profile() {
         <Text></Text>
         
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconAntDesign name='customerservice' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:118}}>
         <Text style={styles.text} >Customer Support</Text>
-        <View style={{paddingLeft:165}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -109,11 +136,13 @@ function profile() {
         <Text></Text>
        
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconAntDesign name='staro' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:155}}>
         <Text style={styles.text} >Rate Our App</Text>
-        <View style={{paddingLeft:200}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
@@ -121,20 +150,21 @@ function profile() {
         <Text></Text>
         
         <TouchableOpacity style={styles.Clicktabs}>
-        <View style={{borderWidth:1}}>   
-        <IconAntDesign name='bars' size ={20} color='grey'/>
+        <View style={styles.leftIcon}>   
+        <IconFeather name='edit' size ={20} color='grey'/>
         </View>
+        <View style={{borderWidth:0,paddingRight:115}}>
         <Text style={styles.text} >Make a Suggestion</Text>
-        <View style={{paddingLeft:160}}> 
+        </View>
+        <View style={styles.rightIcon}> 
         <IconAntDesign name='rightcircleo' size ={20} color='grey'/>
         </View>
         </TouchableOpacity>
         
         </View> 
-
         </View>
-        
         </View>
+        </ScrollView>
     );
   }
 
@@ -160,8 +190,8 @@ const styles = StyleSheet.create({
         paddingLeft:0,
         borderWidth:0,
         borderRadius:10,
-        backgroundColor:'white'
-
+        backgroundColor:'white',
+        
     },
 
     item: {
@@ -198,6 +228,21 @@ const styles = StyleSheet.create({
         borderWidth:1,
         padding:10
     },
+    
+    leftIcon: {
+      borderWidth:0,
+      alignItems:'flex-start',
+      flex:1
+    },
+
+    rightIcon: {
+      paddingLeft:0,
+      flex:1,
+      borderWidth:0,
+      alignItems:'flex-end'
+    },
+
+
 
     Clicktabs :{
 
@@ -206,6 +251,8 @@ const styles = StyleSheet.create({
         borderWidth:0,
         backgroundColor: 'white',
         flexDirection:'row',
+        justifyContent: 'space-evenly'
+      
     },
     
     text:{
